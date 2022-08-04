ed91d9949b4268fccb9c646c91f80ca424614046
zroesch <71677369+zroesch@users.noreply.github.com>
2022-08-03 17:48:19 -0700

    first commit

---
+++ rover_information.js
@@ -0,0 +1,52 @@
+// Information/Descriptions of different rovers that will be used in various parts of the website.
+
+// Perseverance
+
+
+
+// Curiosity
+
+
+
+// Oppurtunity
+var oppurtunity = "Opportunity has worked on Mars longer than any other robot—nearly 15 years. The rover last communicated with Earth on June 10, 2018, as a planet-wide dust storm blanketed the solar-powered rover’s location on Mars. In 2015, Opportunity broke the record for extraterrestrial travel by driving more than the distance of a marathon, with a total of 28.06 miles (45.16 kilometers).";
+
+
+
+
+
+// Spirit
+
+var twins = "Spirit and Opportunity landed on Mars January 3 and January 24, 2004 PST (Jan. 4 and Jan. 25 UTC). Both rovers lived well beyond their planned 90-day missions. Opportunity worked nearly 15 years on Mars and broke the driving record for putting the most miles on the odometer. ";
+
+var landing = "Each rover bounced onto the surface inside a landing craft protected by airbags. When they stopped rolling, the airbags were deflated and the landing craft opened. The rovers rolled out to take panoramic images. These images gave scientists the information they needed to select promising geological targets to tell part of the story of water in Mars' past. Then, the rovers drove to those locations and beyond to perform close-up scientific investigations.";
+
+
+// Cameras
+
+// Front Hazard Avoidance Camera 
+// On Curiosity, Opportunity, and Spirit
+var FHAZ = "Front Hazard Avoidance Camera";
+
+// Rear Hazard Avoidance Camera
+// On Curiosity, Opportunity, and Spirit
+var RHAZ = "Rear Hazard Avoidance Camera";
+
+// Navigation Camera
+var NAVCAM = "Two color stereo Navigation Cameras, called Navcams, help engineers navigate Perseverance safely, particularly when the rover operates autonomously, making its own navigation decisions without consulting controllers on Earth. + Located up high on the rover's mast, these two cameras help engineers drive the rover around Mars. They can see an object as small as a golf ball from 82 feet (25 meters) away. Before Perseverance \"drives blind,\” the navigation cameras initially help ensure a safe path. Blind-drive mode occurs when engineers command the rover to drive a certain distance in a certain direction, and the rover's computer \"brains\" calculate distance from wheel rotations without looking or checking for wheel slippage.";
+
+
+
+//Curiosity Specific Cameras
+// Mast Camera, only on Curiosity
+var MAST = "The Mast Camera, or Mastcam for short, takes color images and color video footage of the Martian terrain. The images can be stitched together to create panoramas of the landscape around the rover.";
+
+// Chemistry and camera Complex
+var CHEMCHAM = "The Chemistry and Camera tool is known as ChemCam. ChemCam's laser, camera and spectrograph work together to identify the chemical and mineral composition of rocks and soils.";
+
+// Mars Hand Lens Imager
+var MAHLI = "The Mars Hand Lens Imager, called MAHLI, is the rover's version of the magnifying hand lens that geologists usually carry with them into the field. MAHLI's close-up images reveal the minerals and textures in rock surfaces.";
+
+// Mars Descent Imager
+var MARDI = "The Mars Descent Imager, nicknamed \"MARDI,\" shot a color video of the terrain below as the rover descended to its landing site. The video helped mission planners select the best path for Curiosity when the rover started exploring Gale Crater.";
+
