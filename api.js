af0f3682a38a75b959fe770fccf9622c0f04d113
zroesch <71677369+zroesch@users.noreply.github.com>
2022-08-10 15:00:51 -0700

    working on paginated sets of images

--- api.js
+++ api.js
@@ -1,54 +1,104 @@
-/**
- * http://mars-photos.herokuapp.com/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz
- * earth_date=2015-6-3
- * page=1
- */
 PHOTO_API = "http://mars-photos.herokuapp.com/api/v1/rovers/";
 
+// const list_items = [
+// 	"Item 1",
+// 	"Item 2",
+// 	"Item 3",
+// 	"Item 4",
+// 	"Item 5",
+// 	"Item 6",
+// 	"Item 7",
+// 	"Item 8",
+// 	"Item 9",
+// 	"Item 10",
+// 	"Item 11",
+// 	"Item 12",
+// 	"Item 13",
+// 	"Item 14",
+// 	"Item 15",
+// 	"Item 16",
+// 	"Item 17",
+// 	"Item 18",
+// 	"Item 19",
+// 	"Item 20",
+// 	"Item 21",
+// 	"Item 22"
+// ];
 
-/**
- * Perseverance Rover
- * EDL_RUCAM 	Rover Up-Look Camera
-    EDL_RDCAM 	Rover Down-Look Camera
-    EDL_DDCAM 	Descent Stage Down-Look Camera
-    EDL_PUCAM1 	Parachute Up-Look Camera A
-    EDL_PUCAM2 	Parachute Up-Look Camera B
-    NAVCAM_LEFT 	Navigation Camera - Left
-    NAVCAM_RIGHT 	Navigation Camera - Right
-    MCZ_RIGHT 	Mast Camera Zoom - Right
-    MCZ_LEFT 	Mast Camera Zoom - Left
-    FRONT_HAZCAM_LEFT_A 	Front Hazard Avoidance Camera - Left
-    FRONT_HAZCAM_RIGHT_A 	Front Hazard Avoidance Camera - Right
-    REAR_HAZCAM_LEFT 	Rear Hazard Avoidance Camera - Left
-    REAR_HAZCAM_RIGHT 	Rear Hazard Avoidance Camera - Right
-    SKYCAM 	MEDA Skycam
-    SHERLOC_WATSON 	SHERLOC WATSON Camera
- */
+const list_items = [
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+]
 
+const list_element = document.getElementById('list');
+const pagination_element = document.getElementById('pagination');
 
+let current_page = 1;
+let rows = 5;
 
-/**
- * Curiosity Rover
- * FHAZ = Front Hazard Avoidance Camera
- * RHAZ  = Rear Hazard Avoidance Camera
- * MAST = Mast Camera
- * CHEMCHAM = Chemistry and Camera Complex
- * MAHLI = Mars Hand Lens Imager
- * MARDI = Mars Descent Imager
- * NAVCAM = Navigation Camera
- */
+function DisplayList (items, wrapper, rows_per_page, page) {
+	wrapper.innerHTML = "";
+	page--;
 
+	let start = rows_per_page * page;
+	let end = start + rows_per_page;
+	let paginatedItems = items.slice(start, end);
 
-/**
- * Oppurtunity and Spirit Rovers
- * FHAZ = Front Hazard Avoidance Camera
- * RHAZ  = Rear Hazard Avoidance Camera
- * NAVCAM = Navigation Camera
- * PANCAM = Panoramic Camera
- * MINITES = Miniature Thermal Emission Spectrometer
- */
+	for (let i = 0; i < paginatedItems.length; i++) {
+		let item = paginatedItems[i];
 
+		let item_element = document.createElement('IMG');
+		// item_element.classList.add('');
+		item_element.src = item;
+		
+		wrapper.appendChild(item_element);
+	}
+}
 
+function SetupPagination (items, wrapper, rows_per_page) {
+	wrapper.innerHTML = "";
 
+	let page_count = Math.ceil(items.length / rows_per_page);
+	for (let i = 1; i < page_count + 1; i++) {
+		let btn = PaginationButton(i, items);
+		wrapper.appendChild(btn);
+	}
+}
 
+function PaginationButton (page, items) {
+	let button = document.createElement('button');
+	button.innerText = page;
 
+	if (current_page == page) button.classList.add('active');
+
+	button.addEventListener('click', function () {
+		current_page = page;
+		DisplayList(items, list_element, rows, current_page);
+
+		let current_btn = document.querySelector('.pagenumbers button.active');
+		current_btn.classList.remove('active');
+
+		button.classList.add('active');
+	});
+
+	return button;
+}
+
+DisplayList(list_items, list_element, rows, current_page);
+SetupPagination(list_items, pagination_element, rows);
